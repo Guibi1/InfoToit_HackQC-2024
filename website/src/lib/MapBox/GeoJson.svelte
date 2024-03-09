a1f29b0f7c94741afd86db1e2f2d96e2a43d414d
Laurent Stéphenne <laurent@guibi.dev>
2024-03-09 00:21:34 -0500

    Added a stupid GeoJson

---
+++ website/src/lib/MapBox/GeoJson.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import mapboxgl, { type AnySourceData } from "mapbox-gl";
+    import { getContext, onMount, setContext } from "svelte";
+    import type { MapContext } from "./Map.svelte";
+
+    export let id: string;
+    export let data: AnySourceData;
+
+    const mapContext = getContext<MapContext>("map");
+    const map = mapContext.getMap();
+
+    onMount(() => {
+        if (map) map.addSource(id, data);
+        return () => map?.removeSource(id);
+    });
+</script>
+
+<slot />
