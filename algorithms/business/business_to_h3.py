5c5e1d66a5e687d89c413ad678bd3ed63de4a91c
Olivier Saint-Vincent <olivier520100@gmail.com>
2024-03-10 14:55:49 -0400

    TOOL IS DONE

--- algorithms/business/business_to_h3.py
+++ algorithms/business/business_to_h3.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import h3
 import json
+import numpy as np
+def get_color(score):
+    # Define the range and midpoint
+    min_score, max_score, midpoint = 0, 10, 0.075
+    # Calculate the proportion of the score in relation to the midpoint
+    if score > midpoint:
+        # For scores above the midpoint, we transition from yellow to green
+        green_component = 255
+        red_component = round(255 * (1 - (score - midpoint) / (max_score - midpoint)))
+    else:
+        # For scores below the midpoint, we transition from red to yellow
+        red_component = 255
+        green_component = round(255 * (score / midpoint))
+    return f'#{red_component:02x}{green_component:02x}00'
 
 
 # Function to convert an H3 index to a GeoJSON polygon
@@ -59,15 +73,16 @@ def h3_index_set_to_file(filename,indexset):
 
     print(f"Created GeoJSON file at {output_geojson_path}")
 
-resolution = 8
+resolution = 9
 h3indexset = geojson_to_h3_index_set("../../data/montreal/montreal.geojson",resolution)
 df = pd.read_csv('../../data/montreal/businesses_cleaned.csv')
 
+businesstypes = set(df["USAGE3"])
+
 h3indexhashmap = {}
 
-
-# for element in h3indexset:
-#     h3indexhashmap[element] = {"description": "", "hasbusinessinfo": False ,"businesses": [], "businesscount": 0}
+for element in h3indexset:
+    h3indexhashmap[element] = {"adjacentindexes": [], "description": "", "hasbusinessinfo": False ,"businesses": [], "businesscount": 0}
 
 for index, row in df.iterrows():
     h3index = h3.geo_to_h3(row["LAT"], row["LONG"],resolution)
@@ -86,16 +101,54 @@ for index, row in df.iterrows():
         h3indexhashmap[h3index] = {"description": "", "businesses": [], "businesscount": 0}
         h3indexhashmap[h3index]["businesses"].append(businessdict)
         h3indexhashmap[h3index]["hasbusinessinfo"] = True
+highestbusinesscount = 0
 
+businesscountlist = []
 
-
-    
 for elements in h3indexhashmap:
+
     h3indexhashmap[elements]["businesscount"] = len(h3indexhashmap[elements]["businesses"])
+    businesscountlist.append(h3indexhashmap[elements]["businesscount"])
+    if (h3indexhashmap[elements]["businesscount"] > highestbusinesscount):
+        highestbusinesscount = h3indexhashmap[elements]["businesscount"]
+    adjacentindex = list(h3.k_ring(elements,1))
+    adjacentindex.remove(elements)
+    h3indexhashmap[elements]["adjacentindexes"] = adjacentindex
+print(np.sum(np.array(businesscountlist)))
+print(np.count_nonzero(np.array(businesscountlist)))
+
+for elements in h3indexhashmap:
+    score = (h3indexhashmap[elements]["businesscount"] / highestbusinesscount) * 10
+    h3indexhashmap[elements]["businessscore"] = score
+    colorchosen = get_color(score)
+    h3indexhashmap[elements]["color"] = colorchosen
     
+print(highestbusinesscount)
 
-print(len(h3indexset))
-print(len(h3indexhashmap))
+geojson_polygons = []
+for index in set(h3indexhashmap):
+    {"type": "Feature", "properties": {}, "geometry": h3_to_geojson_polygon(index) }
 
-h3_index_set_to_file("test.geojson",set(h3indexhashmap))
+    currentdict = {"type": "Feature", "properties": {}, "geometry": h3_to_geojson_polygon(index) }
+    currentdict["properties"] = {
+        "fill": h3indexhashmap[index]["color"],
+        "stroke": h3indexhashmap[index]["color"],
+        "stroke-width": 2,
+        "stroke-opacity": 1,
+        "fill-opacity": 0.5
+      }
+    geojson_polygons.append(currentdict)
+
+# Wrap the polygons in a FeatureCollection
+geojson_feature_collection = {
+    "type": "FeatureCollection",
+    "features": geojson_polygons}
+
+
+# Save the GeoJSON FeatureCollection to a file
+output_geojson_path = "test.geojson"  # replace with your desired path
+with open(output_geojson_path, 'w') as f:
+    json.dump(geojson_feature_collection, f)
+
+print(f"Created GeoJSON file at {output_geojson_path}")
 
