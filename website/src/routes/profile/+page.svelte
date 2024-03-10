d313d4b93a311b381915916b6eebcaa01eb104b0
Laurent Stéphenne <laurent@guibi.dev>
2024-03-09 21:07:06 -0500

    Added dummy homes

--- website/src/routes/profile/+page.svelte
+++ website/src/routes/profile/+page.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
     export let data;
-    data.name="Gabriel Mikus"
-    
+
     let loading = false;
 </script>
 
-<main>
+<main class="container mx-auto py-4">
     <div class="card">
-        <img src={data.avatar} alt="" />
+        <div class="h-24 w-24 rounded border-2 border-dark">
+            <img src={data.avatar} alt="votre avatar" />
+        </div>
+
         <h1 class="h1">{data.name}</h1>
     </div>
-     <h2 class="h1">Maisons sauvegardées</h2>
-    <div class="card m-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {#each data.savedHome as home}
-        <div class=""></div>
+
+    <h2 class="h1">Maisons sauvegardées</h2>
+
+    <div class="m-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {#each data.savedHomes as home}
+            <div class="card">{home.address}</div>
+        {:else}
+            <div>Pas de maisons</div>
         {/each}
     </div>
 </main>
