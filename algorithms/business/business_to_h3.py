0a5b8216a2738ab3de50ae4f890df89513323f81
Olivier Saint-Vincent <olivier520100@gmail.com>
2024-03-09 21:51:22 -0500

    Cleaned up

---
+++ algorithms/business/business_to_h3.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('../../data/montreal/businesses_cleaned.csv')
+
+print(df['USAGE1'])
--- algorithms/geojson/geojson_to_h3grid.py
+++ algorithms/geojson/geojson_to_h3grid.py
@@ -13,45 +13,61 @@ def h3_to_geojson_polygon(h3_index):
     }
     return polygon
 
-# Load the Montreal GeoJSON to create the boundary shape for the H3 polyfill function
-file_path = 'montrealmap.geojson'  # replace with your file path
-with open(file_path, 'r') as file:
-    montreal_geojson = json.load(file)
+def h3_index_set_to_file(filename,indexset):
 
-# Define the resolution for the H3 indexing
-resolution = 9  # You can adjust this resolution as needed
+    geojson_polygons = [h3_to_geojson_polygon(index) for index in indexset]
 
-# Initialize an empty set to store unique H3 indexes for all polygons
-all_h3_indexes = set()
+    # Wrap the polygons in a FeatureCollection
+    geojson_feature_collection = {
+        "type": "FeatureCollection",
+        "features": [
+            {"type": "Feature", "properties": {}, "geometry": poly}
+            for poly in geojson_polygons
+        ]
+    }
+    # Save the GeoJSON FeatureCollection to a file
+    output_geojson_path = filename  # replace with your desired path
+    with open(output_geojson_path, 'w') as f:
+        json.dump(geojson_feature_collection, f)
 
-# Iterate over each feature in the GeoJSON file
-for feature in montreal_geojson['features']:
-    geometry = feature['geometry']
-    if geometry['type'] == 'Polygon':
-        polygons = [geometry]
-    elif geometry['type'] == 'MultiPolygon':
-        polygons = [{"type": "Polygon", "coordinates": coords} for coords in geometry['coordinates']]
+    print(f"Created GeoJSON file at {output_geojson_path}")
+
+def geojson_to_h3_index_set(filename,resolution):
+    # Load the Montreal GeoJSON to create the boundary shape for the H3 polyfill function
+    file_path = filename # replace with your file path
+    with open(file_path, 'r') as file:
+        montreal_geojson = json.load(file)
+
+    # Initialize an empty set to store unique H3 indexes for all polygons
+    all_h3_indexes = set()
+
+    # Iterate over each feature in the GeoJSON file
+    for feature in montreal_geojson['features']:
+        geometry = feature['geometry']
+        if geometry['type'] == 'Polygon':
+            polygons = [geometry]
+        elif geometry['type'] == 'MultiPolygon':
+            polygons = [{"type": "Polygon", "coordinates": coords} for coords in geometry['coordinates']]
+        
+        # Apply polyfill to each Polygon
+        for polygon in polygons:
+            feature_h3_indexes = h3.polyfill(polygon, resolution, geo_json_conformant=True)
+            all_h3_indexes.update(feature_h3_indexes)
+
+    return all_h3_indexes
+    # Convert all H3 indexes to GeoJSON polygons
+
+def geojson_file_to_h3_geojson(filename,resolution):
     
-    # Apply polyfill to each Polygon
-    for polygon in polygons:
-        feature_h3_indexes = h3.polyfill(polygon, resolution, geo_json_conformant=True)
-        all_h3_indexes.update(feature_h3_indexes)
+    
+    filenamein = filename.replace(".geojson","")
+    output_geojson_path = filenamein + f'_h3_tessellation_res_{resolution}.json'  # replace with your desired path
 
-# Convert all H3 indexes to GeoJSON polygons
-geojson_polygons = [h3_to_geojson_polygon(index) for index in all_h3_indexes]
+    indexset = geojson_to_h3_index_set(filename,resolution)
+    h3_index_set_to_file(output_geojson_path,indexset)
+    
+    
 
-# Wrap the polygons in a FeatureCollection
-geojson_feature_collection = {
-    "type": "FeatureCollection",
-    "features": [
-        {"type": "Feature", "properties": {}, "geometry": poly}
-        for poly in geojson_polygons
-    ]
-}
 
-# Save the GeoJSON FeatureCollection to a file
-output_geojson_path = 'montreal_h3_tessellation.geojson'  # replace with your desired path
-with open(output_geojson_path, 'w') as f:
-    json.dump(geojson_feature_collection, f)
 
-print(f"Created GeoJSON file at {output_geojson_path}")
+geojson_file_to_h3_geojson('..\..\data\montreal\\arrondissements.geojson',9)
