911947d7fa7eb368f002673e9ad7dafe531341ec
Laurent Stéphenne <laurent@guibi.dev>
2024-03-10 15:51:46 -0400

    Auth now mostly works!

--- website/src/lib/auth.ts
+++ website/src/lib/auth.ts
@@ -1,4 +1,12 @@
-import { DISCORD_ID, DISCORD_SECRET, GITHUB_ID, GITHUB_SECRET, SECRET,LINKEDIN_ID,LINKEDIN_SECRET } from "$env/static/private";
+import {
+    DISCORD_ID,
+    DISCORD_SECRET,
+    GITHUB_ID,
+    GITHUB_SECRET,
+    LINKEDIN_ID,
+    LINKEDIN_SECRET,
+    SECRET,
+} from "$env/static/private";
 import { getXataClient } from "$xata";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Discord from "@auth/sveltekit/providers/discord";
@@ -9,16 +17,27 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
     providers: [
         GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
         Discord({ clientId: DISCORD_ID, clientSecret: DISCORD_SECRET }),
-        LinkedIn({clientId:LINKEDIN_ID,clientSecret:LINKEDIN_SECRET}),
+        LinkedIn({ clientId: LINKEDIN_ID, clientSecret: LINKEDIN_SECRET }),
     ],
     secret: SECRET,
-    events: {
-        createUser({ user }) {
-            getXataClient().db.Users.create({
-                name: user.name ?? "",
-                email: user.email,
-                avatar: user.image,
-            });
+    callbacks: {
+        signIn({ user }) {
+            getXataClient()
+                .db.Users.select(["id"])
+                .filter({ email: user.email })
+                .getFirst()
+                .then((res) => {
+                    if (res) return;
+                    getXataClient().db.Users.create({
+                        name: user.name ?? "",
+                        email: user.email,
+                        avatar: user.image,
+                    });
+                });
+            return true;
         },
+    },
+    pages: {
+        signIn: "/../login",
     },
 });
